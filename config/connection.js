74df7e220d147e1548466903ff742d2b982b21b0
moonryc <70920813+moonryc@users.noreply.github.com>
2022-02-16 16:27:55 -0600

    MADE CHANGES FOR HEROKU

--- config/connection.js
+++ config/connection.js
@@ -6,8 +6,8 @@ let sequelize;
 
 //create connection to the db
 
-if(process.env.JWSDB_URL){
-    sequelize = new Sequelize(process.env.JWSDB_URL)
+if(process.env.JAWSDB_URL){
+    sequelize = new Sequelize(process.env.JAWSDB_URL)
 }else{
     sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW,{
         host: "localhost",
