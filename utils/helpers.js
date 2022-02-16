54cdca042687c2ee64d9e917543098782fa46866
moonryc <70920813+moonryc@users.noreply.github.com>
2022-02-15 18:53:50 -0600

    added a helper for debugging and added a capitalize_sentences helper, the capitalize helper is not complete yet

---
+++ utils/helpers.js
@@ -0,0 +1,24 @@
+module.exports = {
+    format_date: date => {
+        return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
+            date
+        ).getFullYear()}`;
+    },
+    format_plural: (word, amount) => {
+        if (amount !== 1) {
+            return `${word}s`;
+        }
+
+        return word;
+    },
+    capitalize_sentences:(sentence)=>{
+      const arrayOfWords = sentence.split('')
+      for(let i = 0; i<arrayOfWords.length;i++){
+          arrayOfWords[i] = arrayOfWords[i].charAt(0).toUpperCase() + arrayOfWords[i].slice(1)
+      }
+      return arrayOfWords.join('')
+    },
+    logger:(object)=>{
+        return console.log(object)
+    }
+}
