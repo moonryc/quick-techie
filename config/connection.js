979f386438f861330c6de13a1d412f17d6bf6c6e
moonryc <70920813+moonryc@users.noreply.github.com>
2022-02-17 16:14:29 -0600

    lets user know whats being used

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,17 @@
+require('dotenv').config()
 const Sequelize = require('sequelize')
 
-require('dotenv').config()
 
 let sequelize;
 
 //create connection to the db
 
 if(process.env.JAWSDB_URL){
+    console.log('using jaws')
     sequelize = new Sequelize(process.env.JAWSDB_URL)
 }else{
+    console.log('using local')
+    console.log(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW)
     sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW,{
         host: "localhost",
         dialect: "mysql",
